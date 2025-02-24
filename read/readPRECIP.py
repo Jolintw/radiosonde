e4c809e1866619ac1f30b4c7bc91c77f87829847
Jolin <b07209039@gmail.com.tw>
2025-02-24 16:21:18 +0800

    remove p increasing points of L2 data

--- read/readPRECIP.py
+++ read/readPRECIP.py
@@ -3,4 +3,11 @@ import numpy as np
 def read_sounding_L2(filepath):
     var = {}
     var['time_offset'], var['p'], var['T'], var['rh'], var['ws'], var['wd'], var['z'], var['Td'], var['u'], var['v'] = np.loadtxt(filepath, delimiter=',', skiprows=46, unpack=True, usecols=(1,2,3,4,5,6,9,11,12,13))
+    dp = var["p"][1:] - var["p"][:-1]
+    while np.any(dp>=0):
+        ind = np.arange(dp.shape[0], dtype=int)
+        saveind = np.append([0], ind[dp<0] + 1)
+        for v in var:
+            var[v] = var[v][saveind]
+        dp = var["p"][1:] - var["p"][:-1]
     return var
